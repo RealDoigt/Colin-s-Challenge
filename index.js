c4896b9503b9e847a6f0bd86f16ffacce6be1989
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2022-09-09 14:34:35 -0400

    basic functionality
    man I'm already bored

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
- 
+const plantStates = {
+
+    Seed:         Symbol("seed"),
+    Sprout:       Symbol("sprout"),
+    Fruit:        Symbol("fruit"),
+    RipeFruit:    Symbol("ripe fruit"),
+    RottingFruit: Symbol("rotting fruit"),
+    RottenFruit:  Symbol("rotten fruit"),
+    Nothing:      Symbol("nothing")
+};
+
+let hunger = 0;
+
+const consume = (plant) => {
+
+    switch (plant) {
+
+        case plantStates.Seed:
+        case plantStates.Fruit:
+        case plantStates.RottingFruit: --hunger;      break;
+        case plantStates.Sprout:       ++hunger;      break;
+        case plantStates.RottenFruit:    hunger += 2; break;
+        case plantStates.RipeFruit:      hunger -= 2; break;
+    }
+};
+
+const isStarving = () => hunger >= 20; // game over
